bb9df8a3782a1517792e96579090029c23132062
LLL <2798142644@qq.com>
2026-08-27 15:09:29 +0800

    qoder_2

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 import os, sys
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+current_file_path = os.path.abspath(__file__)
+model_file_path = os.path.dirname(current_file_path)
+root_path = os.path.dirname(model_file_path)
+sys.path.insert(0, root_path)
 
-from RecommendApp import recommend_app
-from BuyApp import buy_app
-from ConsultApp import consult_app
-from KnowledgeApp import knowledge_app
-from UnknownApp import unknown_app
+from app.RecommendApp import recommend_app
+from app.BuyApp import buy_app
+from app.ConsultApp import consult_app
+from app.KnowledgeApp import knowledge_app
+from app.UnknownApp import unknown_app
